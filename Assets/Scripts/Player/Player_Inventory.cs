067a0f7d744a7a55c2f23aa0e8a44b27e1753aed
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2021-03-12 16:23:24 +0100

    Added Shop Script

---
+++ Assets/Scripts/Player/Player_Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Inventory : MonoBehaviour
+{
+
+    [HideInInspector] public enum Items { HEALTPOTION, MONEY };
+    [HideInInspector] public Items item;
+
+    [SerializeField] private int money = 0;
+    [SerializeField] private int numHealthPotions = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(Items _item)
+    {
+        switch(_item)
+        {
+            case (Items.MONEY):
+                money++;
+                break;
+
+            case (Items.HEALTPOTION):
+                numHealthPotions++;
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Shop/Shop_System.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop_System : MonoBehaviour
+{
+    [Header("GameObjects")]
+    [SerializeField] private GameObject noMoneyWin = null;
+    [SerializeField] private GameObject player = null;
+    private GameObject[] items = null;
+
+    [Header("Text")]
+    [SerializeField] private TextMesh money = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noMoneyWin.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player");
+        items = GameObject.FindGameObjectsWithTag("Item_Shop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseNoMoneyWind()
+    {
+        noMoneyWin.SetActive(false);
+    }
+
+}
