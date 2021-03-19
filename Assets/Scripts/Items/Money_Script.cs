d0b066bed9a3fce4a38b43c3cee9ecd5a0a15a67
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2021-03-19 09:33:15 +0100

    Added Money Coin

---
+++ Assets/Scripts/Items/Money_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money_Script : MonoBehaviour
+{
+    private GameObject player = null;
+    private Transform parent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        parent = this.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            player.GetComponent<Player_Inventory>().AddItem(Player_Inventory.Items.MONEY);
+            Destroy(parent.gameObject);
+        }
+    }
+
+}
