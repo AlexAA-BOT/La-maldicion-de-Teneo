555d75913a0334ac50a54f39c5c498771045d681
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2021-04-09 00:05:27 +0200

    Fixed Money Error

--- Assets/Scripts/Items/Money_Script.cs
+++ Assets/Scripts/Items/Money_Script.cs
@@ -19,7 +19,7 @@ public class Money_Script : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
